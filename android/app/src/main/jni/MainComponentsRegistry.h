2d3e73a34498c41c04b99a5561aed36d1b058463
Pablo Neres <pabloneres@hotmail.com>
2022-12-20 11:48:55 -0300

    update

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/integrada/app/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/testeapp/br/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 
 class Home extends React.Component {
 
     componentDidMount() {
+      console.log("teste")
+
+      this.requestPermission()
+
+      messaging().onMessage(async remoteMessage => {
+        Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      });
+
+      messaging().setBackgroundMessageHandler(async remoteMessage => {
+        console.log('Message handled in the background!', remoteMessage);
+      });
+    }
+
+    requestPermission = () => {
+        messaging().requestPermission().then((response) => {
+            this.firebaseInit()
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    firebaseInit() {
+        // Check permission
+        messaging().hasPermission()
+            .then(enabled => {
+                if (enabled) {
+                    // Get firebase token
+                    this.firebaseGetToken();
+                }
+                else {
+                    // Request permission
+                    messaging().requestPermission()
+                        .then(() => {
+                            // Get firebase token
+                            this.firebaseGetToken();
+                        })
+                        .catch(error => {
+                            console.log(error)
+                            // User has rejected permissions
+                        });
+                }
+            });
+    }
+
+    firebaseGetToken() {
+        messaging().getToken().then(token => {
+            console.log(token)
+        });
     }
 
     render() {
